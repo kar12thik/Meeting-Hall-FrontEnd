a6fe5bbc79684886d5e0f58a99f80cfd2cc1aac9
Karthikeyan Subramanian <skarthik.chennai@gmail.com>
2020-06-22 12:00:14 +0530

    Updates

---
+++ precache-manifest.6182ad099befbc3efe9d8cfe59ca4d6b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1a299ef9cdc531d208d3f454fa8dbb2",
+    "url": "/Meeting-Hall-FrontEnd/index.html"
+  },
+  {
+    "revision": "7423c24e2b1af028096d",
+    "url": "/Meeting-Hall-FrontEnd/static/css/2.82ecc6d4.chunk.css"
+  },
+  {
+    "revision": "a70f2d01ba0773a4d64b",
+    "url": "/Meeting-Hall-FrontEnd/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7423c24e2b1af028096d",
+    "url": "/Meeting-Hall-FrontEnd/static/js/2.51155225.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Meeting-Hall-FrontEnd/static/js/2.51155225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a70f2d01ba0773a4d64b",
+    "url": "/Meeting-Hall-FrontEnd/static/js/main.62338a1b.chunk.js"
+  },
+  {
+    "revision": "90cc67ccc65789695932",
+    "url": "/Meeting-Hall-FrontEnd/static/js/runtime-main.0cda5dcf.js"
+  },
+  {
+    "revision": "4f21f4b8fd095bc8a27b1c67d18fb816",
+    "url": "/Meeting-Hall-FrontEnd/static/media/Customer.4f21f4b8.png"
+  },
+  {
+    "revision": "5e0fb5096a7070142a90fdd923941f71",
+    "url": "/Meeting-Hall-FrontEnd/static/media/Earth.5e0fb509.jpg"
+  },
+  {
+    "revision": "2a3f084fca2de90945358c3a8a168bb6",
+    "url": "/Meeting-Hall-FrontEnd/static/media/Mercury.2a3f084f.bin"
+  },
+  {
+    "revision": "5ad4ebe5ca6c64eccf64df1803dc7ee3",
+    "url": "/Meeting-Hall-FrontEnd/static/media/Venus.5ad4ebe5.jpg"
+  }
+]);
